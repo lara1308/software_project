4a2659fc5bf5e25cb29728e31d078aa225c90146
lara1308 <106531005+lara1308@users.noreply.github.com>
2025-09-16 08:35:16 +0900

    Add files via upload

---
+++ 0915.ino
@@ -0,0 +1,28 @@
+#define PIN_LED 7
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(PIN_LED, LOW);
+  delay(1000);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(PIN_LED, LOW);  
+    delay(100);                   
+    digitalWrite(PIN_LED, HIGH);   
+    delay(100); 
+  }
+
+  digitalWrite(PIN_LED, HIGH);
+
+  while (1) {
+  
+  }
+
+
+}
